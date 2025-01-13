be6e57b0591c00cd7f56b40c3a413934e3b6c7a7
Joao Barroso <joao.barroso@alu.ufc.br>
2025-01-13 17:10:16 -0300

    feat: add entities

---
+++ src/main/java/org/edd/model/entities/Transaction.java
@@ -0,0 +1,56 @@
+package org.edd.model.entities;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class Transaction {
+    private String id;
+    private String accountNumber;
+    private BigDecimal amount;
+    private TransactionType type;
+    private LocalDateTime timestamp;
+
+    public enum TransactionType {
+        DEPOSIT, WITHDRAWAL, TRANSFER
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public TransactionType getType() {
+        return type;
+    }
+
+    public void setType(TransactionType type) {
+        this.type = type;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+}
