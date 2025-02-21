07e5290f8942b93ec38e3edfebf33f4c77ddbb8c
Joao Barroso <joao.barroso@alu.ufc.br>
2025-02-21 11:25:04 -0300

    chore: remove JUnit test dependencies from Gradle configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,11 +14,6 @@ repositories {
     mavenCentral()
 }
 
-dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-}
-
 tasks.jar {
     manifest {
         attributes["Main-Class"] = "org.edd.Main"
